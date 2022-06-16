966e9015e6db98e4572feda4b6eb84da1958f539
nkpresh <nkwuap@gmail.com>
2022-06-17 00:46:48 +0100

    Movement Sound fix

--- Assets/Scripts/CharacterAnimationEvents.cs
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -8,12 +8,25 @@ public class CharacterAnimationEvents : MonoBehaviour
 
     public AudioClip aud_footStep;
     public AudioSource audio;
+    public CharacterMovement characterMovement;
     void Start()
     {
 
     }
-    public void FootStep()
+    public void FootStep(float targetWalkSpeed)
     {
-        audio.PlayOneShot(aud_footStep);
+
+        float actualSpeed = characterMovement.Velocity.magnitude;
+        if (GetMovementState(targetWalkSpeed) == GetMovementState(actualSpeed))
+        {
+            audio.PlayOneShot(aud_footStep);
+        }
+    }
+    private int GetMovementState(float speed)
+    {
+        if (speed < .5f) return 0;
+        if (speed < 3f) return 1;
+        if (speed < 6f) return 2;
+        return 3;
     }
 }
