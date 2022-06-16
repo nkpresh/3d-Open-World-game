2c640215b1bf5ddfe53bd72ef4a4561a9377fcc2
nkpresh <nkwuap@gmail.com>
2022-06-16 23:50:18 +0100

    Audio foos steps added

---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+
+    public AudioClip aud_footStep;
+    public AudioSource audio;
+    void Start()
+    {
+
+    }
+    public void FootStep()
+    {
+        audio.PlayOneShot(aud_footStep);
+    }
+}
